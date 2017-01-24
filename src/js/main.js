bab8f80ef866b8d94a0904f1f9559d43057ce192
Joshua Heagle <joshuaheagle@gmail.com>
2017-01-24 00:07:57 -0500

    Fix curry function so that it can use functions with more than 2 parameters. Clean up some of the code to use the curry function. Work on simplifying. Also, when building arrays and matrices ensure that the arrays and objects are copied by value not references.

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,16 @@
 "use strict";
 (function() {
-  // const curry = (fn) => function curried(...args){ return args.length >= fn.length ? fn(...args) : (a) => curried(...[...args, a]);}
-  const curry = (fn) => function curried(...args){
-    if (args.length >= fn.length) {
-      return fn(...args)
-    } else {
-      return (a) => curried(...[...args, a]);
-    }
-  }
+  const curry = (fn) => function curried(...args){ return args.length >= fn.length ? fn(...args) : (...a) => curried(...[...args, ...a]);}
 
-  const bTile = {
+  const tile = {
     hasShip: false,
     isHit: false,
     styles: {
       border: '1px solid #333',
       width: '24px',
       height: '24px',
-    }
+    },
+    element: {},
   }
 
   const shipTile = {
@@ -28,12 +22,15 @@
   }
 
   const fillArray = (a, i, l) => {
-    a.push(JSON.parse(JSON.stringify(i))); // create a new version of item so it is not linked by reference
-    return (--l > 0 ? fillArray(a, i, l) : a.slice());
+    // create a new version of item so it is not linked by reference
+    a = a.slice();
+    i = JSON.parse(JSON.stringify(i));
+    a.push(i);
+    return (--l > 0 ? fillArray(a, i, l) : a);
   }
-  const initArray = (i, l) => fillArray([], i, l);
-  const buildArray = curry(initArray)
-  const lineContent = buildArray(bTile);
+  const initArray = curry(fillArray);
+  const buildArray = initArray([]);
+  const lineContent = buildArray(tile);
   const rectMatrix = (x, y) => buildArray(lineContent(x), y);
   const rect3d = (x, y, z) => buildArray(rectMatrix(x, y), z);
   const squareMatrix = (size) => rectMatrix(size, size);
@@ -58,53 +55,64 @@
     if (Object.keys(config.styles).length){
       Object.keys(config.styles).forEach((styleName) => el.style[styleName] = config.styles[styleName]);
     }
+    if (!config.element.length){
+      config.element = el;
+    }
   }
 
-  const recurseHtml = (types, matrix, el, depth) => {
-    var el = el || {};
+  const recurseHtml = (types, matrix, depth) => {
     var depth = depth || 0;
     let nextDepth = (i) => nextIndex(types, i);
-    if (!(el instanceof window.Element)){
-      el = document.createElement(Array.isArray(types[depth]) ? types[depth][types[depth].length - 1] : types[depth]);
-      el = recurseHtml(types, matrix, el, nextDepth(depth));
+    if (depth === 0){
+      let elems = [];
       if (Array.isArray(types[depth])){
-        let elem = document.createElement(types[depth][0]);
-        elem.appendChild(el);
-        return elem;
+        elems = types[depth].map((elem) => document.createElement(elem));
       }
-      return el;
+      matrix.forEach((a) => elems[elems.length - 1].appendChild(recurseHtml(types, a, nextDepth(depth))));
+      let prevElem = false;
+      elems.forEach((elem) => {
+        if (prevElem){
+          prevElem.appendChild(elem);
+        }
+        prevElem = elem;
+      });
+      return elems[0];
     }
     if (Array.isArray(matrix)){
-      matrix.forEach((arr) => {
-        el.appendChild(recurseHtml(types, arr, document.createElement(types[depth]), nextDepth(depth)));
-      });
+      let el = document.createElement(types[depth]);
+      matrix.forEach((a) => el.appendChild(recurseHtml(types, a, nextDepth(depth))));
       return el;
     }
-    el = document.createElement(types[depth])
+    let el = document.createElement(types[depth]);
     if (matrix instanceof Object){
       configureHtml(el, matrix);
     }
     return el;
   }
 
-  const createHtml = (types, matrix) => recurseHtml(types, matrix, {}, 0);
+  const createHtml = (types, matrix) => recurseHtml(types, matrix, 0);
   const generateHtml = curry(createHtml);
   const tableConfig = [['table', 'tbody'], 'tr', 'td'];
   const createTable = generateHtml(tableConfig);
 
   const updateCell = (matrix, config, x, y) => Object.assign(matrix[y][x], config);
+  const alterCell = curry(updateCell);
   const update3dCell = (matrix, config, x, y, z) => Object.assign(matrix[y][x][z], config);
-  const boardUbdate = (config, x, y) => updateCell(board, config, x, y);
-  const alterBoard = curry(boardUbdate);
-  alterBoard(shipTile, 2, 3);
-  alterBoard(shipTile, 2, 4);
-  alterBoard(shipTile, 2, 5);
-  alterBoard(shipTile, 2, 6);
-  alterBoard(shipTile, 2, 7);
-  alterBoard(hitTile, 2, 5);
-  alterBoard(hitTile, 3, 5);
+  const alter3dCell = curry(update3dCell);
+  const updateBoard = alterCell(board);
+  const setShip = updateBoard(shipTile);
+  const setHit = updateBoard(hitTile);
+  setShip(2, 3);
+  setShip(2, 4);
+  setShip(2, 5);
+  setShip(2, 6);
+  setShip(2, 7);
+  setHit(2, 5);
+  setHit(3, 5);
 
   const matrixTable = createTable(board);
+
+  // console.log(board);
   document.body.appendChild(matrixTable);
 
 }());
