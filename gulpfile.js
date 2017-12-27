786e5cc61d99fb01fef4ea85d2816a5a12cb4d50
Joshua Heagle <joshuaheagle@gmail.com>
2017-12-27 11:46:12 -0500

    Update indentation of one comment in the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -57,7 +57,7 @@ gulp.task('useref', function () {
 // Optimizing Images
 gulp.task('images', function () {
   return gulp.src('src/img/**/*.+(png|jpg|jpeg|gif|svg)')
-    // Caching images that ran through imagemin
+  // Caching images that ran through imagemin
     .pipe(cache(imagemin({
       interlaced: true
     })))
